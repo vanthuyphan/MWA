9168b9833bf8466134e58401114daaf95102abc4
Van Phan <vanthuyphan@gmail.com>
2018-05-29 15:02:43 -0500

    update lap1

--- lap1/exercise2.js
+++ lap1/exercise2.js
@@ -1,31 +1,18 @@
-Array.prototype.isDual = function(){
-    if (this.length == 0) return 1
-    const hasEvenNumberOfElements = (this.length % 2) == 0;
-    if (!hasEvenNumberOfElements) {
-        return 0;
-    } else {
-        const sum = this[0] + this[1];
-        for (let i = 2 ; i < this.length; i = i + 2) {
-            let nextSum = this[i] + this[i + 1];
-            if (nextSum != sum) {
-                return 0;
-            }
-        }
-        return 1;
-    }
+const dayType = {WEEKEND: "weekend", WEEKDAY: "weekday"}
+const dayMap = {
+    0: dayType.WEEKDAY,
+    1: dayType.WEEKDAY,
+    2: dayType.WEEKDAY,
+    3: dayType.WEEKDAY,
+    4: dayType.WEEKDAY,
+    5: dayType.WEEKEND,
+    6: dayType.WEEKEND
+}
+const isWeekend = () => {
+    const today = new Date();
+    const day = today.getDay();
+    return dayMap[day];
 }
 
-const test1 = [1, 2, 3, 0].isDual();
-const test2 = [1, 2, 2, 1, 3, 0].isDual();
-const test3 = [1, 1, 2, 2].isDual();
-const test4 = [1, 2, 3].isDual();
-const test5 = [].isDual();
-if (test1 == 1 && test2 == 1 && test3 == 0 && test4 == 0 && test5 == 1) {
-    console.log("All test cases passed");
-} else {
-    console.log("Test1", test1);
-    console.log("Test2", test2);
-    console.log("Test3", test3);
-    console.log("Test4", test4);
-    console.log("Test5", test5);
-}
+const test1 = isWeekend();
+console.log(test1);
--- lap1/exercise3.js
+++ lap1/exercise3.js
@@ -1,31 +1,21 @@
-Array.prototype.isDual = function(){
-    if (this.length == 0) return 1
-    const hasEvenNumberOfElements = (this.length % 2) == 0;
-    if (!hasEvenNumberOfElements) {
-        return 0;
-    } else {
-        const sum = this[0] + this[1];
-        for (let i = 2 ; i < this.length; i = i + 2) {
-            let nextSum = this[i] + this[i + 1];
-            if (nextSum != sum) {
-                return 0;
-            }
+const applyCoupon = (category) => {
+    return (discount) => {
+        return (item) => {
+             item.price = (item.price * (1 - discount));
+             return item;
         }
-        return 1;
     }
-}
 
-const test1 = [1, 2, 3, 0].isDual();
-const test2 = [1, 2, 2, 1, 3, 0].isDual();
-const test3 = [1, 1, 2, 2].isDual();
-const test4 = [1, 2, 3].isDual();
-const test5 = [].isDual();
-if (test1 == 1 && test2 == 1 && test3 == 0 && test4 == 0 && test5 == 1) {
-    console.log("All test cases passed");
+}
+const item = {
+    "name": "Biscuits",
+    "type": "regular",
+    "category": "food",
+    "price": 2.0
+}
+const test1 = applyCoupon("food")(0.1)(item).price;
+if (test1 == 1.8) {
+    console.log("Test passed")
 } else {
-    console.log("Test1", test1);
-    console.log("Test2", test2);
-    console.log("Test3", test3);
-    console.log("Test4", test4);
-    console.log("Test5", test5);
+    console.log("Test failed")
 }
